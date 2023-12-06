1ca2200289264597f35ad63d1ae32728e27cebfe
Ahasainul <adnan31615asif@gmail.com>
2023-12-06 23:50:09 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,37 @@
+tailwind.config = {
+    theme: {
+      extend: {
+    //  /colour start
+    colors: {
+        blue: '#0872b9',
+        gay2: '#f1f1f1',
+        gay: '#828282',
+        futter: '#353535',
+        overlay: ' rgba(0, 0, 0, 0.6)',
+      },
+    //  /colour end
+    // width start 
+
+    maxWidth: {
+        'container': '1170px',
+      },
+    // width end
+    // background-img-start 
+    backgroundImage: {
+      'bgimages': "url('./images/bg.jpg')",
+      
+    },
+
+    // background-img-end
+// linehite start 
+lineHeight: {
+  'line': '24px',
+
+}
+// linehite end
+
+
+
+      }
+    }
+  }
